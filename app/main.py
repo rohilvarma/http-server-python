9337a080613dd2411f6ad31a192224566dc2ef04
rohilvarma <rohilvarma96@gmail.com>
2024-07-13 02:17:40 +0530

    Fixed response body returned.

--- app/main.py
+++ app/main.py
@@ -9,13 +9,18 @@ def check_endpoints(endpoint: str) -> tuple[bool, str]:
     echo_route_value = re.split(_available_endpoints[2], endpoint)
     return endpoint in _available_endpoints or echo_route_value[0] == '', echo_route_value[1] if len(echo_route_value)>1 else ""
     
+def get_http_response(http_code: int, route_path_value: str="") -> bytes:
+    _http_responses = {
+        200: f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(route_path_value[1:])}\r\n\r\n{route_path_value[1:]}",
+        404: "HTTP/1.1 404 Not Found\r\n\r\n"
+    }
+    if http_code == 200 and route_path_value == "":
+        return b"HTTP/1.1 200 OK\r\n\r\n"
+    
+    return _http_responses[http_code].encode()
+
 def main():
     print("Logs from your program will appear here!")
-
-    _http_responses = {
-        200: b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n",
-        404: b"HTTP/1.1 404 Not Found\r\n\r\n"
-    }
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
@@ -36,21 +41,14 @@ def main():
             print(log_message, request_target)
             print("$$$$$$$$$$$$$$")
             
-            if request_available:
-                if route_path != "":
-                    # Taking from 1 index to omit the '/'
-                    client_socket.send(_http_responses[200] + route_path[1:].encode())
-                else:
-                    client_socket.send(_http_responses[200])
-            else:
-                client_socket.send(_http_responses[404])
-            client_socket.send(_http_responses[200] if request_available else _http_responses[404])
+            http_response = get_http_response(200 if request_available else 404, route_path)
+            return client_socket.send(http_response)
 
         except Exception as e:
             print("Error encountered", e)
             
-        # client_socket.close()    
-        # print("Connection closed!")
+        client_socket.close()    
+        print("Connection closed!")
 
 if __name__ == "__main__":
     main()

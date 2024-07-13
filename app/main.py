ae21fdb19926c08df831d84424e535ad0b980902
rohilvarma <rohilvarma96@gmail.com>
2024-07-13 11:28:12 +0530

    Refactored code and added response for /user-agent.

--- app/main.py
+++ app/main.py
@@ -1,56 +1,59 @@
 import socket
-import re
 
-
-def check_endpoints(endpoint: str) -> tuple[bool, str]:
-    _available_endpoints = [
-        "/", "/index.html", "/echo"
-    ]
-    echo_route_value = re.split(_available_endpoints[2], endpoint)
-    return endpoint in _available_endpoints or echo_route_value[0] == '', echo_route_value[1] if len(echo_route_value)>1 else ""
+def get_accessed_endpoint(request_line: str) -> str:
+    return request_line.split(" ")[1]
     
-def get_http_response(http_code: int, route_path_value: str="") -> bytes:
-    _http_responses = {
-        200: f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(route_path_value[1:])}\r\n\r\n{route_path_value[1:]}",
-        404: "HTTP/1.1 404 Not Found\r\n\r\n"
-    }
-    if http_code == 200 and route_path_value == "":
+def get_http_response(endpoint: str, headers: dict[str, str]) -> bytes:
+    if endpoint == "/" or endpoint == "/index.html":
         return b"HTTP/1.1 200 OK\r\n\r\n"
+    elif endpoint.startswith("/echo"):
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}".format(len(endpoint[6:]), endpoint[6:])
+        return response.encode()
+    elif endpoint == "/user-agent":
+        user_agent_value = headers["User-Agent"]
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}".format(len(user_agent_value), user_agent_value)
+        return response.encode()
     
-    return _http_responses[http_code].encode()
+    return b"HTTP/1.1 404 Not Found\r\n\r\n"
+    
+def create_headers(request_list: list[str]) -> dict[str, str]:
+    header_keys = ["Host", "User-Agent", "Accept"]
+    
+    headers = {}
+    for header in request_list:
+        if ":" in header:
+            key, value = header.split(": ")
+            if key in header_keys:
+                headers[key] = value
+                
+    return headers
 
-def main():
+def main() -> None:
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    
     while True:
-        client_socket, client_address = server_socket.accept() # wait for client
+        client_socket, client_addr = server_socket.accept()
+        print("New client connected!")
+        
         try:
-            request = client_socket.recv(1024).decode('utf-8')
-            print("Request received.")
+            request = client_socket.recv(1024).decode()
+            request_list = request.split("\r\n")
             
-            # Extracting the Request line of the HTTP request.
-            request_line = request.split('\r\n')[0]
+            endpoint = get_accessed_endpoint(request_list[0])
             
-            # Extracting the request target value of the HTTP request.
-            request_target = request_line.split(" ")[1]
+            headers = create_headers(request_list[1:])
             
-            request_available, route_path = check_endpoints(request_target)
+            client_socket.send(get_http_response(endpoint, headers))
             
-            log_message = "Request Target is" if request_available else "Invalid Request Target."
-            print(log_message, request_target)
-            print("$$$$$$$$$$$$$$")
-            
-            http_response = get_http_response(200 if request_available else 404, route_path)
-            return client_socket.send(http_response)
-
         except Exception as e:
-            print("Error encountered", e)
-            
-        client_socket.close()    
-        print("Connection closed!")
-
+            print("Exception occured", e)
+    
+        finally:
+            client_socket.close()
+            print("Connection is closed!\n")
+    
+    
 if __name__ == "__main__":
     main()
-    # a, b = check_endpoints('/echo/abc')
-    # print(a, b)

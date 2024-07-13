1d235fa0a37f5b67cf548e379dfa5699d9269a04
rohilvarma <rohilvarma96@gmail.com>
2024-07-13 12:58:13 +0530

    Added support for concurrent connections.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
 import socket
+import threading
+
 
 def get_accessed_endpoint(request_line: str) -> str:
+    """
+    Extracts the endpoint from the HTTP request line.
+    
+    :param request_line: The request line of HTTP request.
+    :returns: Endpoint from the request line, will return an empty string if endpoint doesn't exist.
+    :rtype: str
+    """
     return request_line.split(" ")[1]
     
 def get_http_response(endpoint: str, headers: dict[str, str]) -> bytes:
+    """
+    Determines the HTTP response based on the endpoint accessed.
+    
+    :param endpoint: The endpoint requested by HTTP.
+    :param headers: Headers dictionary containing all headers from the request.
+    :returns: Byte response for the HTTP request.
+    :rtype: bytes
+    """
     if endpoint == "/" or endpoint == "/index.html":
         return b"HTTP/1.1 200 OK\r\n\r\n"
     elif endpoint.startswith("/echo"):
@@ -17,6 +34,13 @@ def get_http_response(endpoint: str, headers: dict[str, str]) -> bytes:
     return b"HTTP/1.1 404 Not Found\r\n\r\n"
     
 def create_headers(request_list: list[str]) -> dict[str, str]:
+    """
+    Method to create a headers dictionary from the HTTP request.
+    
+    :param request_list: HTTP request split by CRLF, contains header and body.
+    :returns: A headers dictionary.
+    :rtype: dict[str, str]
+    """
     header_keys = ["Host", "User-Agent", "Accept"]
     
     headers = {}
@@ -28,32 +52,36 @@ def create_headers(request_list: list[str]) -> dict[str, str]:
                 
     return headers
 
+def handle_clients(client_socket: socket.socket):
+    try:
+        request = client_socket.recv(1024).decode()
+        request_list = request.split("\r\n")
+        
+        endpoint = get_accessed_endpoint(request_list[0])
+        
+        headers = create_headers(request_list[1:])
+        
+        client_socket.send(get_http_response(endpoint, headers))
+        
+    except Exception as e:
+        print("Exception occured", e)
+
+    finally:
+        client_socket.close()
+        print("Connection is closed!\n")
+
 def main() -> None:
     print("Logs from your program will appear here!")
     
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True, backlog=3)
     
     while True:
         client_socket, client_addr = server_socket.accept()
         print("New client connected!")
         
-        try:
-            request = client_socket.recv(1024).decode()
-            request_list = request.split("\r\n")
-            
-            endpoint = get_accessed_endpoint(request_list[0])
-            
-            headers = create_headers(request_list[1:])
-            
-            client_socket.send(get_http_response(endpoint, headers))
-            
-        except Exception as e:
-            print("Exception occured", e)
-    
-        finally:
-            client_socket.close()
-            print("Connection is closed!\n")
-    
+        connection_handler = threading.Thread(target=handle_clients, args=(client_socket, ))
+        connection_handler.start()
+
     
 if __name__ == "__main__":
     main()
